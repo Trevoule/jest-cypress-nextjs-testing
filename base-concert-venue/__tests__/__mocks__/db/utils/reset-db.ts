c34025f80bce9b93a47f60173814f311aed98e6c
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2022-12-22 18:17:10 +0100

    reset function for test db

---
+++ base-concert-venue/__tests__/__mocks__/db/utils/reset-db.ts
@@ -0,0 +1,32 @@
+// file that will read data from json files from fakeData
+// and will write it to db json files
+import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+import { filenames, writeJSONToFile } from "@/lib/db/db-utils";
+
+// method that  will reset data fro db
+// async method as we need to read file
+export const resetDB = async () => {
+  // failsafe against resetting production db!
+  const safeToReset = process.env.NODE_ENV === "test";
+
+  if (!safeToReset) {
+    console.log(
+      "WARNING: database reset unavailable outside test environment!"
+    );
+    return;
+  }
+
+  //  read files
+  const { fakeShows, fakeBands, fakeUsers, fakeReservations } =
+    await readFakeData();
+
+  // overwrite data in files
+  // filenames would be known from function 'getDbPath'
+  // file created according to enum 'filenames' types
+  await Promise.all([
+    writeJSONToFile(filenames.bands, fakeBands),
+    writeJSONToFile(filenames.shows, fakeShows),
+    writeJSONToFile(filenames.users, fakeUsers),
+    writeJSONToFile(filenames.reservations, fakeReservations),
+  ]);
+};
