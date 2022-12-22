7e0fd28be24ab593ee92e79d9b9737d20cd2bab1
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2022-12-22 13:43:52 +0100

    testing static page

---
+++ base-concert-venue/__tests__/ui/home.test.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-unresolved */
+import { render, screen } from "@testing-library/react";
+
+import Home from "@/pages/index";
+
+test("page has correct heading and image", () => {
+  render(<Home />);
+
+  const heading = screen.getByRole("heading", {
+    name: /welcome to popular concert venue/i,
+  });
+
+  expect(heading).toBeInTheDocument();
+
+  const image = screen.getByRole("img", {
+    name: "Concert goer with hands in the shape of a heart",
+  });
+
+  expect(image).toBeInTheDocument();
+});
