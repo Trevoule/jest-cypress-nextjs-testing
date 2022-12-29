8f0ad7805a56b50f788f69f1fbad91463ec87497
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2022-12-29 18:16:02 +0100

    Testing Routes with Query String Params

---
+++ base-concert-venue/__tests__/api/bands.test.ts
@@ -0,0 +1,16 @@
+import { testApiHandler } from "next-test-api-route-handler";
+import bandsHandler from "@/pages/api/bands/index";
+
+test("POST api/bands returns 401 status for incorrect revalidation secret", async () => {
+  await testApiHandler({
+    handler: bandsHandler,
+    paramsPatcher: (params) => {
+      // eslint-disable-next-line no-param-reassign
+      params.queryStringURLParams = { secret: "NOT SECRET" };
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: "POST" });
+      expect(res.status).toBe(401);
+    },
+  });
+});
